c0e1f479fbf61ce09c6cbeef8792699d964bbe43
cac19148 <68085111+cac19148@users.noreply.github.com>
2020-07-31 23:31:30 +0200

    Update Tabla-2-POS.v

--- Tabla_2_POS/Tabla-2-POS.v
+++ Tabla_2_POS/Tabla-2-POS.v
@@ -46,7 +46,7 @@ initial begin	// noten el uso del begin y end
 end	
 initial
     begin
-      $dumpfile("Tabla-2-POS.v"); // noten que el nombre del archivo siempre es el que incluye el _tb y la extension es .vcd
+      $dumpfile("Tabla-2-POS_tb.vcd"); // noten que el nombre del archivo siempre es el que incluye el _tb y la extension es .vcd
       $dumpvars(0, Tabla2POS); // noten que el nombre que está después del número 0 es el mismo que el nombre del módulo que estamos probando
     end
 endmodule	//finalizar el módulo
