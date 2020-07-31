823804ea40fca0653dfef58786c32aaf6783846e
cac19148 <68085111+cac19148@users.noreply.github.com>
2020-07-31 23:31:05 +0200

    Update Tabla-1-POS.v

--- Tabla_1_POS/Tabla-1-POS.v
+++ Tabla_1_POS/Tabla-1-POS.v
@@ -1,77 +1,50 @@
-$date
-	Fri Jul 31 20:20:15 2020
-$end
-$version
-	Icarus Verilog
-$end
-$timescale
-	1ps
-$end
-$scope module Tabla1POS $end
-$var wire 1 ! NA $end
-$var wire 1 " NB $end
-$var wire 1 # NC $end
-$var wire 1 $ P1 $end
-$var wire 1 % S1 $end
-$var wire 1 & S2 $end
-$var wire 1 ' S3 $end
-$var reg 1 ( A $end
-$var reg 1 ) B $end
-$var reg 1 * C $end
-$upscope $end
-$enddefinitions $end
-#0
-$dumpvars
-0*
-0)
-0(
-1'
-1&
-1%
-1$
-1#
-1"
-1!
-$end
-#1
-0$
-0%
-0#
-1*
-#2
-1$
-1#
-1%
-0"
-0*
-1)
-#3
-0$
-0&
-0#
-1*
-#4
-1$
-1#
-1"
-1&
-0!
-0*
-0)
-1(
-#5
-0#
-1*
-#6
-0$
-0'
-1#
-0"
-0*
-1)
-#7
-1$
-1'
-0#
-1*
-#8
+// -----------------------------------
+// Fernando José Caceros Morales (19148)
+// Basado en: http://www.asic-world.com/verilog/verilog_one_day1.html#Low_level_design
+// Tabla 1 POS 
+// -----------------------------------
+module Tabla1POS();	// generar y nombrar un módulo
+  
+
+wire NA,NB,NC,S1,S2,S3,S4,S5,P1;
+
+reg A,B,C;	
+
+// podemos utilizar compuertas nativas   
+
+not NotA (NA, A);
+not NotB (NB, B);
+not NotC (NC, C);
+
+or Or1 (S1, A,B,NC);
+or Or2 (S2, A,NB,NC);
+or Or3 (S3, NA,NB,NC);
+
+and And1 (P1, S1,S2,S3);		
+
+// initial sirve para definir eventos desde el tiempo t = 0
+
+initial begin	// noten el uso del begin y end
+  //monitor nos permite rastrear cambios en las entradas y salidas
+  // %b = binario %d = decimal %h = hex  \t = tabulación
+  $monitor("%b %b %b | %b", A, B, C, P1);
+  // Declaramos valores en el tiempo 0
+  A = 0;
+  B = 0;
+  C = 0;
+  
+  #1 C = 1;
+  #1 B = 1; C=0;
+  #1 C = 1;
+  #1 A=1;B=0;C=0;
+  #1 C=1;
+  #1 B=1;C=0;
+  #1 C=1;
+  #1 $finish;   // termina la simulación
+end	
+initial
+    begin
+      $dumpfile("Tabla-1-POS_tb.vcd"); // noten que el nombre del archivo siempre es el que incluye el _tb y la extension es .vcd
+      $dumpvars(0, Tabla1POS); // noten que el nombre que está después del número 0 es el mismo que el nombre del módulo que estamos probando
+    end
+endmodule	//finalizar el módulo
